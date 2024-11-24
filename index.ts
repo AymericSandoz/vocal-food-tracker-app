4f0b742aa3d214c996375363f80c6dbc18285bf8
AymericSandoz <sandoz.aymeric@gmail.com>
2024-11-24 22:59:37 +0100

    master Add new hooks, components, and color constants for improved theming and UI functionality

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 import { speechToText } from "./functions/speechToText";
-// import cors from "cors";
+import cors from "cors";
 import "dotenv/config";
 
 const port = process.env.PORT ? Number(process.env.PORT) : 4000;
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 
 // Cross-origin requests
-// app.use(cors());
+app.use(cors());
 
 app.post("/speech-to-text", (req: Request, res: Response) => {
   speechToText(req, res);
